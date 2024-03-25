b3350734448930e3f09d38dfe72c159a9ecedffb
janet.aoko <janet.aoko@TBG-KEN-DTP-071>
2024-03-25 11:54:06 +0300

    add

---
+++ Movie/Controllers/RatingController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Movie.Models;
+
+namespace Movie.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class RatingController : ControllerBase
+	{ private readonly MovieDb _context;
+		public RatingController (MovieDb context)
+		{
+			_context = context;
+		}
+		[HttpPost]
+		[Route("Create")]
+		public async Task<IActionResult> Create(Rating rating)
+		{
+			var Rating = new Rating();
+			{
+				Rating.DateCreated = DateTime.Now;
+				Rating.User = rating.User;
+			}
+			_context.Add(Rating);
+			await _context.SaveChangesAsync();
+			return Ok(Rating);
+		}
+	}
+}
